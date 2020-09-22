bbdadcdc0aad5fe4bf8d3e836e9c0c2f6d82a2fa
Anket17 <anketnemlekar17@gmail.com>
2020-09-22 23:41:03 +0530

    first commit

--- client/src/Context/AuthContext.js
+++ client/src/Context/AuthContext.js
@@ -18,7 +18,7 @@ export default ({children})=>{
 
     return(
         <div>
-            {!isLoaded? <h1>Loading</h1>:
+            {!isLoaded? <h1> Loadinggggggg</h1>:
             <AuthContext.Provider value={{user,setUser, isAuthenticated}}>
             {children}
             </AuthContext.Provider>}
